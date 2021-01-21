fb9994462e0fb6b0db03d52787783706b9053275
SevakOhanian7 <sevakohanian7@gmail.com>
2021-01-21 12:30:30 -0800

    changes to unit_test

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,14 +1,9 @@
-#include "gtest/gtest.h"
+nclude "gtest/gtest.h"
 
-#include "sub_test.hpp"
-#include "add_test.hpp"
 #include "op_test.hpp"
 #include "rand_test.hpp"
-#include "div_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
-
